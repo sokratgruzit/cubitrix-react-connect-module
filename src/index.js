d48622f32e268b966cfec756d8c00824ffd297b8
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-06-12 16:01:24 +0400

    last

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { connectReducer } from "./reducers/connectReducer.js";
 export { stakeReducer } from "./reducers/stakeReducer.js";
 export { useConnect } from "./hooks/use-connect.js";
 export { useStake } from "./hooks/use-stake.js";
+export { INIT_STATE as STAKE_INIT_STATE } from "./reducers/stakeReducer.js";
