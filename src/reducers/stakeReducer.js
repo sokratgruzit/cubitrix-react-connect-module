751efdde40d7f6e9eac460e9f0d2b44fdbad1aa5
gogaokradze <deathofsuper@gmail.com>
2023-07-04 13:13:46 +0400

    fix

--- src/reducers/stakeReducer.js
+++ src/reducers/stakeReducer.js
@@ -32,7 +32,7 @@ export const stakeReducer = (state = INIT_STATE, { type, payload }) => {
     };
   }
 
-  if ((type = "UPDATE_STAKERS_RECORD")) {
+  if (type === "UPDATE_STAKERS_RECORD") {
     return {
       ...state,
       stakersRecord: [...state.stakersRecord, ...payload],
