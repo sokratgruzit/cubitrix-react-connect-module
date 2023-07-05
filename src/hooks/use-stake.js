bc7cc7db475ef110aec9dbf9dd6d8a81ac76346e
gogaokradze <deathofsuper@gmail.com>
2023-07-05 17:42:35 +0400

    fix

--- src/hooks/use-stake.js
+++ src/hooks/use-stake.js
@@ -4,33 +4,9 @@ import STACK_ABI from "../abi/stack.json";
 import WBNB from "../abi/WBNB.json";
 import moment from "moment";
 
-// import { INIT_STATE } from "../reducers/stakeReducer";
+import { INIT_STATE } from "../reducers/stakeReducer";
 
 import { useWeb3React } from "@web3-react/core";
-
-const INIT_STATE = {
-  stackContractInfo: {
-    totalStakers: 0,
-    totalStakedToken: 0,
-  },
-  stakersInfo: {
-    totalStakedTokenUser: 0,
-    totalUnstakedTokenUser: 0,
-    totalClaimedRewardTokenUser: 0,
-    currentStaked: 0,
-    realtimeReward: 0,
-    stakeCount: 0,
-    alreadyExists: false,
-  },
-  depositAmount: "",
-  timeperiod: 0,
-  balance: 0,
-  stakersRecord: [],
-  isAllowance: false,
-  loading: false,
-  hasMoreData: false,
-  timeperiodDate: moment().add(30, "days").format("DD/MM/YYYY h:mm A"),
-};
 
 export const useStake = ({ Router, tokenAddress }) => {
   let { account, library } = useWeb3React();
